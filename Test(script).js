d2a4ca773960847e3c1249d0291744ad8a162e30
adilhanar7 <adilhanarbudu7@gmail.com>
2021-07-28 16:47:11 +0300

    Add mobile version footer , about , min-service , catalog , min-hero , social ,max-index

---
+++ Test(script).js
@@ -0,0 +1,32 @@
+//let
+
+let name = 'Анель';
+
+name = 'Айдана';
+
+console.log(name);
+
+//const 
+
+const title = 'Let';
+
+// title = 'Айдана';
+
+console.log(title);
+
+
+//age
+
+let age = 3;
+let sum = 3 * age + age /  2 + 3 **2;
+
+let sum2 = 2 + '2' * 2;
+
+console.log(sum);
+console.log(sum2);
+
+//list
+
+let names = ['I','You','They'];
+
+console.log(names);
